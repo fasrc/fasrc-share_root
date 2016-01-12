93027136f3332b90a57ce756578144582cce1f91
nate <n8@adullmoment.com>
2016-01-12 15:18:15 -0500

    added defined type

---
+++ manifests/init.pp
@@ -0,0 +1,8 @@
+# Class: share_root
+# defined type adds share root folder and readme for an export
+# don't use this class to create anything, but use either
+# create_resources() or ensure_resources elsewhere
+#
+class share_root {
+}
+
---
+++ manifests/readme.pp
@@ -0,0 +1,31 @@
+# Define: share_root::readme
+# create the share_root directory and
+# README.yaml files from a template
+#
+define share_root::readme (
+  $date        = generate('/bin/date', '+%Y-%d-%m'),
+  $description = 'a new share',
+  $path        = undef,
+  $size        = undef,
+  $ticket      = 'n/a',
+  $owner = 'root',
+  $group = 'root',
+  $mode = '0644',
+
+) {
+  file { "${path}/share_root" :
+    ensure  => directory,
+    owner   => $owner,
+    group   => $group,
+    mode    => $mode,
+  }
+
+  file { "${path}/README.yaml" :
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('share_root/readme.yaml.erb'),
+    require => File[$path],
+  }
+}
